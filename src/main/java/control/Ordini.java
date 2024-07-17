6cc8417835bd806bdfab12ba4a6c13732db8158b
ErricoAquino <errico.aquino24@gmail.com>
2024-07-17 19:19:37 +0200

    car_zone

--- src/main/java/control/Ordini.java
+++ src/main/java/control/Ordini.java
@@ -47,4 +47,5 @@ public class Ordini extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
+    
 }
